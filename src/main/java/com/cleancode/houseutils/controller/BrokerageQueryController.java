e0d2ad5e86b7d7a29aa0819b32a013ef04432681
Claire Jung <dev.jungclaire@gmail.com>
2021-10-11 22:52:57 +0900

    중개수수료 계산하는 로직 추가

---
+++ src/main/java/com/cleancode/houseutils/controller/BrokerageQueryController.java
@@ -0,0 +1,19 @@
+package com.cleancode.houseutils.controller;
+
+import com.cleancode.houseutils.constants.ActionType;
+import com.cleancode.houseutils.policy.BrokeragePolicy;
+import com.cleancode.houseutils.policy.BrokeragePolicyFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BrokerageQueryController {
+
+    @GetMapping("/api/calc/brokerage")
+    public Long calcBrokerage(@RequestParam ActionType actionType,
+                              @RequestParam Long price) {
+        BrokeragePolicy policy = BrokeragePolicyFactory.of(actionType);
+        return policy.calculate(price);
+    }
+}
