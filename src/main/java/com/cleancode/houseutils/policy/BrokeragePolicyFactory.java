49b36c3e11dd33aab7cb425ade88ccb8d4906baf
Claire Jung <dev.jungclaire@gmail.com>
2021-10-27 21:39:05 +0900

    exception 처리 수정

--- src/main/java/com/cleancode/houseutils/policy/BrokeragePolicyFactory.java
+++ src/main/java/com/cleancode/houseutils/policy/BrokeragePolicyFactory.java
@@ -1,6 +1,8 @@
 package com.cleancode.houseutils.policy;
 
 import com.cleancode.houseutils.constants.ActionType;
+import com.cleancode.houseutils.exception.ErrorCode;
+import com.cleancode.houseutils.exception.HouseUtilsException;
 
 public class BrokeragePolicyFactory {
 
@@ -11,7 +13,7 @@ public class BrokeragePolicyFactory {
             case PURCHASE:
                 return new PurchaseBrokeragePolicy();
             default:
-                throw new IllegalArgumentException("해당 actionType 에 대한 정책이 존재하지 않습니다.");
+                throw new HouseUtilsException(ErrorCode.INVALID_REQUEST, "해당 actionType 에 대한 정책이 존재하지 않습니다.");
         }
     }
 }
